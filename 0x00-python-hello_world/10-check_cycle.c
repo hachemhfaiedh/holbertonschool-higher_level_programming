e50c7fe71cad5e866e661e7d6dbdf7dd88d0d2f6
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-04-27 21:46:50 +0000

    check_sycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * check_cycle - checks if a singly linked list has a cycle
+ * @list: singly linked list
+ * Return: 0 if cycle not found, else 1
+ */
+int check_cycle(listint_t *list)
+{
+}
