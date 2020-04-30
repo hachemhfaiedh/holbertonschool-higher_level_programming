ab0d8290da5c82d4c211a9f21f5c197120fbf5fe
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-04-30 22:13:14 +0000

    infinite_add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+from sys import argv
+inf = 0
 if __name__ == '__main__':
-import sys
-a = 0
-    for i in range(1, len(sys.argv)):
-        a += int(sys.argv[i])
-    print("{}".format(a)))
+    for i in range(1, len(argv)):
+        inf += int(argv[i])
+    print("{}".format(inf))
