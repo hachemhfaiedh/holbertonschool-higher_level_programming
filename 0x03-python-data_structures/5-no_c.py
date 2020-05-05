495d0f89c00aa50da80d7709b3ef8a46221194b3
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-05-05 22:02:16 +0000

    no_c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@ def no_c(my_string):
     new_string = ""
     if my_string:
         for char in range(len(my_string)):
-            if char not in 'Cc':
+            if new_string[char] not in 'Cc':
                 new_string = new_string + my_string[char]
     return (new_string)
