1172af0cca856a27dce37d5fd94115de8aa41e55
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-06-16 15:47:21 +0000

    square4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""defining classes"""
+
+
+class Square:
+    """square class with no attributes"""
+    def __init__(self, size=0):
+        """Instantiation with size"""
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """square area"""
+        return self.__size * self.__size
