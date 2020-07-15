5c41a846f5dc6333e729d0c58e33864710cf9b04
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-07-15 09:09:20 +0000

    genre_id_by_show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
-SELECT *
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres
-ON tv_show_genres.show_id = tv_shows.id
+ON tv_shows.title = tv_show_genres.genre_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
