1818f96ed8020934a6a97e24aaf29ca8a74aa5e4
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-07-15 09:33:03 +0000

    genre_id_all_shows

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- script that lists all shows contained in the database hbtn_0d_tvshows.
+SELECT *
+FROM tv_shows
+LEFT JOIN tv_shows_genres
+ON tv_shows.title, tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
