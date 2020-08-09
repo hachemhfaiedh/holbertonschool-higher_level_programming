8051c1abaca90ca9030b0a3b4906c5ea2e15d361
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-08-09 19:31:10 +0000

    adv_fix

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """script that lists all State objects from the database hbtn_0e_6_usa"""
 from sys import argv
-from relationship_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+from relationship_state import State, Base
 from relationship_city import City
 
 
