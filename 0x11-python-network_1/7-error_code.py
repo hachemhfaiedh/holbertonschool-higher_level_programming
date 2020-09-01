23dd9b2bc3ba5e49fe87fd150ea6f5e0284cd45a
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-09-01 11:16:53 +0000

    code_fix

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,7 +8,7 @@ from sys import argv
 
 if __name__ == "__main__":
     re = requests.get(argv[1])
-    if r.status_code >= 400:
+    if re.status_code >= 400:
         print("Error code: {}".format(re.status_code))
     else:
         print(re.text)
