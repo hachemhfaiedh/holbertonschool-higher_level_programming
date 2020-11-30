4db24ca2528ebe80e88ed0c2886c8fb07cfe386c
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-11-30 13:10:32 +0000

    save_to_json_file

---
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""input output"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """save to json file"""
+    with open(filename, 'w') as f:
+        return json.dump(my_obj, f)
