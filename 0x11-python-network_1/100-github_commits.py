8526b195dff82280a7bc7731f8b7d43bb4c99f8b
hachemhfaiedh <hechemhfaiedh@gmail.com>
2021-01-12 02:13:39 -0800

    pep8_fix

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-list 10 commits (from the most recent to oldest) 
+list 10 commits (from the most recent to oldest)
 of the repository “rails” by the user “rails”
 """
 if __name__ == "__main__":
